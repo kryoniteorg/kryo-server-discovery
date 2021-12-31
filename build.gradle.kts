29d4eb7286d3b94de74b4d8d30e2578e23ee8569
Lukas Jost <lukas.jost@outlook.com>
2021-12-31 13:36:59 +0100

    fix: match velocity versions of different sourcesets

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,15 +19,15 @@ repositories {
 
 dependencies {
     implementation("io.fabric8:kubernetes-client:5.11.1")
-    compileOnly("com.velocitypowered:velocity-api:3.1.0")
-    annotationProcessor ("com.velocitypowered:velocity-api:3.0.1")
+    compileOnly("com.velocitypowered:velocity-api:3.1.1")
+    annotationProcessor("com.velocitypowered:velocity-api:3.1.1")
 
     testImplementation("org.slf4j:slf4j-simple:1.7.32")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation("org.mockito:mockito-junit-jupiter:4.2.0")
-    testImplementation("com.velocitypowered:velocity-api:3.0.1")
-    testAnnotationProcessor ("com.velocitypowered:velocity-api:3.0.1")
+    testImplementation("com.velocitypowered:velocity-api:3.1.1")
+    testAnnotationProcessor("com.velocitypowered:velocity-api:3.1.1")
 }
 
 tasks.test {
