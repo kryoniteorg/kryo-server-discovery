ddbc323ce6dbbbeccf97dc11c805c3958b3dac53
Kevin Huber <kevin.huber@hotmail.com>
2021-12-31 06:13:16 +0100

    Fix checkstyle

--- src/test/java/org/kryonite/kryoserverdiscovery/KryoServerDiscoveryPluginTest.java
+++ src/test/java/org/kryonite/kryoserverdiscovery/KryoServerDiscoveryPluginTest.java
@@ -1,19 +1,16 @@
 package org.kryonite.kryoserverdiscovery;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.proxy.ProxyServer;
-import java.util.Timer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.kryonite.kryoserverdiscovery.listener.PlayerJoinListener;
 import org.mockito.Answers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
