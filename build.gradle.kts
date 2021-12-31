5db0f40236e69aa5c38bdb203652ace1d8568b24
Lukas Jost <lukas.jost@outlook.com>
2021-12-31 14:02:46 +0100

    fix: Extract dependency versions into variable for simpler updating (#5)
    * fix: match velocity versions of different sourcesets
    
    * fix: extract versions into variable

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,17 +17,21 @@ repositories {
     maven(url = "https://nexus.velocitypowered.com/repository/maven-public/")
 }
 
+// Dependency versions
+val JUNIT_VERSION by extra("5.7.2")
+val VELOCITY_VERSION by extra("3.1.0")
+
 dependencies {
     implementation("io.fabric8:kubernetes-client:5.11.1")
-    compileOnly("com.velocitypowered:velocity-api:3.1.0")
-    annotationProcessor ("com.velocitypowered:velocity-api:3.0.1")
+    compileOnly("com.velocitypowered:velocity-api:$VELOCITY_VERSION")
+    annotationProcessor("com.velocitypowered:velocity-api:$VELOCITY_VERSION")
 
     testImplementation("org.slf4j:slf4j-simple:1.7.32")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$JUNIT_VERSION")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$JUNIT_VERSION")
     testImplementation("org.mockito:mockito-junit-jupiter:4.2.0")
-    testImplementation("com.velocitypowered:velocity-api:3.0.1")
-    testAnnotationProcessor ("com.velocitypowered:velocity-api:3.0.1")
+    testImplementation("com.velocitypowered:velocity-api:$VELOCITY_VERSION")
+    testAnnotationProcessor("com.velocitypowered:velocity-api:$VELOCITY_VERSION")
 }
 
 tasks.test {
