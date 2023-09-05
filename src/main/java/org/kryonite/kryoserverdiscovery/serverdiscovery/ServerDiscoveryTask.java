3d47129ac75083501d888b7161b82328c75c5356
belohnung <belohnung@protonmail.com>
2023-09-05 09:59:51 +0200

    fix: don't hardcode the container port to 25565
    Signed-off-by: belohnung <belohnung@protonmail.com>

--- src/main/java/org/kryonite/kryoserverdiscovery/serverdiscovery/ServerDiscoveryTask.java
+++ src/main/java/org/kryonite/kryoserverdiscovery/serverdiscovery/ServerDiscoveryTask.java
@@ -2,20 +2,16 @@ package org.kryonite.kryoserverdiscovery.serverdiscovery;
 
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.ServerInfo;
-import io.fabric8.kubernetes.api.model.Namespace;
-import io.fabric8.kubernetes.api.model.NamespaceList;
-import io.fabric8.kubernetes.api.model.Pod;
-import io.fabric8.kubernetes.api.model.PodList;
+import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+
 import java.net.InetSocketAddress;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.TimerTask;
+import java.util.*;
 import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.kryonite.kryoserverdiscovery.KryoServerDiscoveryPlugin;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -61,11 +57,26 @@ public class ServerDiscoveryTask extends TimerTask {
 
   private Set<ServerInfo> getServerInfo(List<Pod> pods) {
     return pods.stream()
-        .map(pod -> new ServerInfo(
-                pod.getMetadata().getName(),
-                new InetSocketAddress(pod.getStatus().getPodIP(), 25565)
-            )
-        )
-        .collect(Collectors.toSet());
+      .map(pod -> {
+        List<ContainerPort> ports = pod.getSpec()
+          .getContainers()
+          .stream()
+          .map(ct -> ct.getPorts().stream().filter(port -> port.getName().equals("minecraft")).findFirst())
+          .filter(Optional::isPresent)
+          .map(Optional::get)
+          .toList();
+
+        String serverName = pod.getMetadata().getName();
+        // Probably a misconfiguration if there is more than one container with a minecraft port
+        if (ports.size() == 1) {
+          ContainerPort port = ports.get(0);
+          return new ServerInfo(serverName, new InetSocketAddress(pod.getStatus().getPodIP(), port.getHostPort()));
+        }
+
+        log.warn(String.format("None or multiple containers in pod '%s' have a port named 'minecraft', using default port 25565", pod.getMetadata().getName()));
+
+        return new ServerInfo(serverName, new InetSocketAddress(pod.getStatus().getPodIP(), 25565));
+      })
+      .collect(Collectors.toSet());
   }
 }
